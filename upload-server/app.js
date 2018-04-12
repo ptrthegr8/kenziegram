d07c7c93753ba727574630660d0f3b75ef07bfed
Peter Marsh <p.marsh@mac.com>
2018-04-12 15:20:34 -0400

    added HTML into js file and css into public dir

--- upload-server/app.js
+++ upload-server/app.js
@@ -9,25 +9,59 @@ const app = express();
 
 const items = [];
 // code that was provided
-app.use(express.static('./public'), );
+app.use('/public', express.static('./public'));
 //
 app.listen(port);
 //
 app.post('/public/uploads', upload.single('myFile'), function (req, res, next) {
     // req.file is the `myFile` file
     // req.body will hold the text fields, if there were any
-    console.log("Uploaded: " + req.file.filename);
+    console.log('Uploaded: ' + req.file.filename);
     items.push(req.file.filename);
-    res.end(`Uploaded file: ${req.file.filename}`);
+    res.send(`Uploaded file: ${req.file.filename} <button onclick="goBack();">back</button> <script>
+    function goBack() {
+        window.history.back();
+    }
+    </script>`);
     //
 });
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
     const path = './public/uploads';
     fs.readdir(path, function (err, items) {
         console.log(items);
-        res.send(`<h1>Welcome to Kenziegram!</h1>`);
+        // let stringCollection = `<h1>KenzieGram</h1>`;
+        let stringCollection = `<!DOCTYPE html>
+        <html>
+        
+        <head>
+            <meta charset="utf-8" />
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <title>Page Title</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link rel="stylesheet" type="text/css" media="screen" href="./public/main.css" />
+        </head>
+        
+        <body>
+            <header>
+                <h1>KenzieGram</h1>
+            </header>
+            <form action="http://localhost:3000/public/uploads" method="POST" enctype="multipart/form-data">
+                <fieldset>
+                    <legend>Where the Uploads Are:</legend>
+                    <div id="file-container">
+                        <input type="file" name="myFile" id="myFile">
+                    </div>
+                    <div id="button-container">
+                        <button>Upload</button>
+                    </div>
+                </fieldset>
+            </form>
+        </body>
+        
+        </html>`;
+        for (let i = 0; i < items.length; i++) {
+            stringCollection += `<img style='border:1px solid black;' src='${path}/${items[i]}'></img>`
+        };
+        res.send(stringCollection);
     });
 });
-app.get('/public/uploads', (req, res, next) => {
-    res.send(items);
-})
